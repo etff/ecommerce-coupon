cbb626a2396057187a576737926e0fd06960445d
etff <wish2rich@gmail.com>
2023-04-22 19:59:32 +0900

    init project

---
+++ src/main/kotlin/com/example/ecommercecoupon/EcommerceCouponApplication.kt
@@ -0,0 +1,11 @@
+package com.example.ecommercecoupon
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class EcommerceCouponApplication
+
+fun main(args: Array<String>) {
+    runApplication<EcommerceCouponApplication>(*args)
+}
---
+++ src/test/kotlin/com/example/ecommercecoupon/EcommerceCouponApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.example.ecommercecoupon
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class EcommerceCouponApplicationTests {
+
+    @Test
+    fun contextLoads() {
+    }
+
+}
